4fc99fbc8f3c35567cf1eda339f297eeda1012b7
safwan <safwankilinakkode53@gmail.com>
2026-02-11 20:21:26 +0530

    cli

---
+++ frontent/main.js
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  const indexPath = path.join(__dirname, "dist", "index.html");
+  win.loadFile(indexPath);
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
