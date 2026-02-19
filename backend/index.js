16d0137675e7245f09fc7dfb9edc9f60a6fe1ebe
safwan <safwankilinakkode53@gmail.com>
2026-02-19 13:04:00 +0530

    origin

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,36 @@
-import express from 'express'
-import mongoose, { Mongoose } from 'mongoose'
-import dotenv from 'dotenv'
-import fileroutes from "./route/fileRoute.js"
-import cors from 'cors'
+import express from "express";
+import mongoose, { Mongoose } from "mongoose";
+import dotenv from "dotenv";
+import fileroutes from "./route/fileRoute.js";
+import cors from "cors";
 
-import dns from "dns";
-dns.setDefaultResultOrder("ipv4first");
-
-
-
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 const startServer = async () => {
   try {
-   await mongoose.connect(process.env.mongoUri, {
-  family: 4
-});
-    console.log("Connected to DB")
+    await mongoose.connect(process.env.mongoUri, {
+      family: 4,
+    });
+    console.log("Connected to DB");
 
     app.listen(process.env.PORT, () => {
-      console.log(`Server started on ${process.env.PORT}`)
-    })
-
+      console.log(`Server started on ${process.env.PORT}`);
+    });
   } catch (error) {
-    console.log("DB Connection Error:", error.message)
+    console.log("DB Connection Error:", error.message);
   }
-}
+};
 
-startServer()
+startServer();
 
-app.use(cors());
-app.use(express.json())
-app.use("/api",fileroutes)
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use("/api", fileroutes);
